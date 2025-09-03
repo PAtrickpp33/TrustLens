9d9bc54f069bfdd77777f1f6c5c17dbed7e4784d
brotherhl6180dw <richardnvl.canlas@gmail.com>
2025-09-04 01:18:34 +1000

    data: email insertion script via REST api

---
+++ backend/data/crud/insert_email.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# To run: 
+# host:TrustLens user$ cd backend
+# host:backend user$ python -m data.crud.insert_email
+
+from .config import settings
+from .utils import get_dataset, chunk_records, batch_post
+
+API_URL = f"{settings.api_url}/email/import"
+
+def main():
+    # Ex. dataset = "email_phishing_clean.parquet"
+    email_data = get_dataset("**/email*clean.parquet")
+
+    # Reformat DF to address, risk_level, notes, mx_valid, and disposable cols
+    email_data = email_data[["address", "risk_level"]].copy()
+    email_data["notes"] = ""
+    email_data["mx_valid"] = 0
+    email_data["disposable"] = 0
+
+    # Convert to JSON array [{record 1}, {record 2}, ...]
+    email_records = email_data.to_dict(orient="records")
+    
+    email_batches = chunk_records(email_records, chunk_size=500)
+
+    # Send POST request to API to update remote DB
+    batch_post(email_batches, API_URL, verbose=True)
+    
+if __name__ == "__main__":
+    main()
