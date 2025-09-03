e5ca36457e4b37983f20ae687fda2134de1218a4
brotherhl6180dw <richardnvl.canlas@gmail.com>
2025-09-04 01:09:07 +1000

    fix: added static path to data/config.py

--- backend/data/crud/config.py
+++ backend/data/crud/config.py
@@ -1,8 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+ENV_PATH = Path(__file__).resolve().parent / ".env"
 
 # Settings for API_url for posting and getting data
 class Settings(BaseSettings):
     api_url: str | None = None
-    model_config = SettingsConfigDict(env_prefix="", env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_prefix="", env_file=str(ENV_PATH), extra="ignore")
     
 settings = Settings()
