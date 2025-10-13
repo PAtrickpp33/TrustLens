e7b5f99c8cfec8b726f1c32a85b64a5d1fe63b9d
Khushi Khera <khushikhera@Khushis-MacBook-Air.local>
2025-10-14 02:13:44 +1100

    Fix: removed unused Link import to fix build error

--- frontend/src/pages/LandingDashboard.tsx
+++ frontend/src/pages/LandingDashboard.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import Papa from "papaparse";
 
